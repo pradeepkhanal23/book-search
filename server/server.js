2fde3d24a61f6cf584b47bad5eec7ace16295d82
Pradeep Khanal <pradeepkhanal642@gmail.com>
2024-07-27 18:59:17 +1000

    restapi routes deleted and new /graphql route added with the needed server configuration

--- server/server.js
+++ server/server.js
@@ -1,21 +1,48 @@
+// node core modules
 const express = require("express");
 const path = require("path");
+
+// imported apollo related contents including the server and the middleware
+const { ApolloServer } = require("@apollo/server");
+const { expressMiddleware } = require("@apollo/server/express4");
+const { typeDefs, resolvers } = require("./schemas");
+
+// connection file
 const db = require("./config/connection");
-const routes = require("./routes");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+// configuring the server with type defs and resolvers
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
 
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
-}
+//starting the server on starr
+const startApolloServer = async () => {
+  await server.start();
 
-app.use(routes);
+  //making sure that our server is able to parse JSON file and url encoded form datas
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
+
+  // if we're in production, serve client/build as static assets
+  if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../client/build")));
+
+    //serving the index.html file on other route except "/graphql"
+    app.get("*", (req, res) => {
+      res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+    });
+  }
+};
+
+// contrary to traditional RESTApi sever, where we create a routes folder and pass it onto the middleware, we are using a single endpoint "graphql" and passing it to the express middleware which has a graphql server implementation (this is special express server imported from apollo to support graphql)
+app.use("/graphql", expressMiddleware(server));
 
 db.once("open", () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
 });
+
+startApolloServer();
