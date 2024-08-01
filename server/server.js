c78132f7a033cfbb3a10522830f857e3439a0141
Pradeep Khanal <pradeepkhanal642@gmail.com>
2024-08-01 19:57:49 +1000

    project being build and served

--- server/server.js
+++ server/server.js
@@ -39,7 +39,7 @@ const startApolloServer = async () => {
 
   // if we're in production, serve client/build as static assets
   if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../client/build")));
+    app.use(express.static(path.join(__dirname, "../client/dist")));
 
     //serving the index.html file on other route except "/graphql"
     app.get("*", (req, res) => {
